9e9b80a338c83b61ac2d7970f1482d22586a8bc8
gzaros <templatewebvo@gmail.com>
2023-10-11 12:41:57 -0700

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,46 @@
 #!/usr/bin/python3
-""" define basemodel class """
+'''module base_model'''
+
 
 import uuid
 from datetime import datetime
+import models
 
 
-class BaseModel:
-    """define it attribute"""
-
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
-
+class BaseModel():
+    '''class BaseModel'''
     def __init__(self, *args, **kwargs):
+        '''class constructor for class BaseModel'''
         if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
             for key, value in kwargs.items():
                 if key != '__class__':
-                    setattr(kwargs, key, value)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
-    def __str__(self, *args, **kwargs):
-        return "[{}] ({}) {}".format(
-        self.__class__.__name__,
-        self.id, 
-        self.__dict__)
+    def __str__(self):
+        '''string of BaseModel instance'''
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
+        '''updates 'updated_at' instance with current datetime'''
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        dic = self.__dict__.copy()
-        dic["__class__"] = self.__class__.__name__
-        dic["created_at"] = self.created_at.isoformat()
-        dic["updated_at"] = self.updated_at.isoformat()
-        return dic
+        '''dictionary representation of instance'''
+        new_dict = dict(self.__dict__)
+        new_dict['created_at'] = self.__dict__['created_at'].isoformat()
+        new_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return (new_dict)
+
