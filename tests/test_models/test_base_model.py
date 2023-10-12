b7e3486f957de922dc14a3cd8e695b0afa112477
hepona <hepona10@gmail.com>
2023-10-13 00:44:16 +0100

    unittest basemode : wip

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,15 +1,59 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+""" unittest for class BaseModel """
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
- 
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+class TestBaseModel(unittest.TestCase):
+    def test_Inequality_Of_Two_Id(self):
+        """ test inequality of two id's """
+        f = FileStorage()
+        b1 = BaseModel()
+        b2  = BaseModel()
+
+        f.new(b1)
+        f.new(b2)
+
+        self.assertNotEqual(b1.id, b2.id)
+    
+    def test_inequality_of_two_instance(self):
+        "Inequality of two instances of BaseModel"
+        b1=BaseModel()
+        b2=BaseModel()
+        self.assertNotEqual(b1, b2)
+
+    def test_if_id_is_str(self):
+        "test id the id is a string"
+        b1=BaseModel()
+        
+        self.addTypeEqualityFunc(b1.id , str)
+    
+    def test_printing_formatt(self):
+        "test if printing the instance of BaseModel\
+equals [<class name>] (<self.id>) <self.__dict__>"
+        b = BaseModel()
+        exceptation = "[BaseModel] ({}) {}".format(b.id, b.__dict__)
+        self.assertEqual(exceptation, str(b))
+
+    def test_if_basmodel_originatFrom_obj_class(self):
+        "check if basemodel originate from objct class"
+        b = BaseModel()
+        self.assertIsInstance(b, object)
+
+    def test_inequality_of_updateAt_createdAt(self):
+        "check if update_at is difference from created_at after saving"
+        f = FileStorage()
+        b = BaseModel()
+        # c = HBNBCommand()
+        f.new(b)
+
+        b.id =  "00000"
+        self.assertNotEqual(b.created_at, b.updated_at)
+
+        # c.do_update("BaseModel", b.id, id, "00000")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
