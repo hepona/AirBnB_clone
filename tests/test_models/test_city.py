893b33448f8da2e9a74a153d999f9b0e5ec11990
hepona <hepona10@gmail.com>
2023-10-14 18:17:40 +0100

    pycodestyle

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,12 @@
+##!/usr/bin/python3
+""" unittest for class city """
+
+import unittest
+
+
+class TestCity(unittest.TestCase):
+    """unitest for city"""
+
+
+if __name__ == "__main__":
+    unittest.main()
